1514a982c34379a85abb321da751e8de1d423244
Stipane <stipane1@gmail.com>
2023-08-03 15:52:51 +0800

    update 03-08-2023

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client'; // Connects the React Package to the DOM (Document Object Model)
+import './index.css'; // All the CSS code for the page
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // This displays our app.js in the index.html file line 31 (div id #root)
 root.render(
   <React.StrictMode>
     <App />
